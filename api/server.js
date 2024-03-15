27813383a6b58bb19487776fcd1f0da631a2ac4d
Chetn11 <cnpatil00@gmail.com>
2024-03-15 12:38:18 +0530

    cors error fixed

--- api/server.js
+++ api/server.js
@@ -5,7 +5,12 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(cors())
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
+
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
